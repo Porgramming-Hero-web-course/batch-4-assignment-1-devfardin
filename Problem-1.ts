ae3e6392b96ebb848aaff8a25b9df5b1133af1a3
devfardin <contactfardin22@gmail.com>
2024-11-07 15:20:36 +0600

    solved problem 3

--- Problem-1.ts
+++ Problem-1.ts
@@ -6,10 +6,10 @@
 
 // Problem 1:
 
-const sumArray = (sumNumber: number[]) => {
+const sumArray = (sumNumber: number[])=> {
   // return sumNumber;
   return sumNumber.reduce((sum, currentNumber) => sum + currentNumber, 0);
 };
 // call function
-const result = sumArray([1, 2, 3, 4, 5]);
-console.log(result);
+const result1 = sumArray([1, 2, 3, 4, 5]);
+console.log(result1);
--- Problem-2.ts
+++ Problem-2.ts
@@ -1,1 +1,8 @@
 // Problem 2:
+
+const removeDuplicates = (numbers: number[]): number[] => {
+  return numbers.filter((number, index) => numbers.indexOf(number) === index);
+};
+const result2 = removeDuplicates([1, 2, 2, 3, 4, 4, 5]);
+
+console.log(result2);
