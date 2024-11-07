bb301c08b80657f68b43467685eda5f862c64d6c
devfardin <contactfardin22@gmail.com>
2024-11-07 16:52:53 +0600

    fixed

--- Problem-3.ts
+++ Problem-3.ts
@@ -1,23 +1,14 @@
 // Problem 3:
 
-// Sample Input:
-// countWordOccurrences("I love typescript", "typescript");
-
-// Sample Output:
-// 1;
-
-interface Counter {
-  sentence: string;
-  word: string;
-}
-
 const countWordOccurrences = (sentence: string, word: string): number => {
   const lowerCaseSectence = sentence.toLocaleLowerCase();
   const lowerCaseWord = word.toLocaleLowerCase();
   const sentenceInWord = lowerCaseSectence.split(" ");
+
   const count = sentenceInWord.filter(
     (wordCounters: string) => wordCounters == lowerCaseWord
   ).length;
+
   return count;
 };
 const result3 = countWordOccurrences("I love typescript", "typescript");
