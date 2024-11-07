5371aef2dc137defd1932e59ebbd3899143afc0e
devfardin <contactfardin22@gmail.com>
2024-11-08 02:35:14 +0600

    problem to re check and fixed issue

--- Problem-1.ts
+++ Problem-1.ts
@@ -5,5 +5,6 @@
     return sumNumber.reduce((sum, currentNumber) => sum + currentNumber, 0);
   };
   const result1 = sumArray([1, 2, 3, 4, 5]);
+  
   console.log(result1);
 }
--- Problem-2.ts
+++ Problem-2.ts
@@ -6,5 +6,5 @@
   };
   const result2 = removeDuplicates([1, 2, 2, 3, 4, 4, 5]);
 
-  console.log(result2);
+  console.log(result2); 
 }
