d63ef9d8dcd3f5addd656c91b758d5e9af3a8c30
devfardin <contactfardin22@gmail.com>
2024-11-07 16:51:02 +0600

    solved problem number 3

--- Problem-3.ts
+++ Problem-3.ts
@@ -5,3 +5,21 @@
 
 // Sample Output:
 // 1;
+
+interface Counter {
+  sentence: string;
+  word: string;
+}
+
+const countWordOccurrences = (sentence: string, word: string): number => {
+  const lowerCaseSectence = sentence.toLocaleLowerCase();
+  const lowerCaseWord = word.toLocaleLowerCase();
+  const sentenceInWord = lowerCaseSectence.split(" ");
+  const count = sentenceInWord.filter(
+    (wordCounters: string) => wordCounters == lowerCaseWord
+  ).length;
+  return count;
+};
+const result3 = countWordOccurrences("I love typescript", "typescript");
+
+console.log(result3);
