8d7fc51414a11b82561d4a6694f81b4f09089737
devfardin <contactfardin22@gmail.com>
2024-11-07 15:02:40 +0600

    create file 2

--- Problem-1.ts
+++ Problem-1.ts
@@ -7,9 +7,9 @@
 // Problem 1:
 
 const sumArray = (sumNumber: number[]) => {
-    // return sumNumber;
-   return sumNumber.reduce( (sum, currentNumber)=>  sum + currentNumber,0)
-}
+  // return sumNumber;
+  return sumNumber.reduce((sum, currentNumber) => sum + currentNumber, 0);
+};
 // call function
 const result = sumArray([1, 2, 3, 4, 5]);
 console.log(result);
